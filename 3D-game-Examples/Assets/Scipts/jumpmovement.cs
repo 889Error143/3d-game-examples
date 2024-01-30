8cd838b687046ea3fe333024cc638862926f11a9
889Error143 <113727616+889Error143@users.noreply.github.com>
2024-01-30 14:23:03 -0800

    finish Movement examples

---
+++ 3D-game-Examples/Assets/Scipts/jumpmovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpmovement : MonoBehaviour
+{
+    public float JumpForce = 10f;
+    public bool IsOnGround = true;
+    private Rigidbody _playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        {
+            _playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            IsOnGround = false;
+        }
+    }
+}
