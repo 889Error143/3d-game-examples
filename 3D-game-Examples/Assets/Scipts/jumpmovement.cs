85271f37699eb95733f037c645cad6a2fdd4ab84
889Error143 <113727616+889Error143@users.noreply.github.com>
2024-02-07 13:33:04 -0800

    jump 3

--- 3D-game-Examples/Assets/Scipts/jumpmovement.cs
+++ 3D-game-Examples/Assets/Scipts/jumpmovement.cs
@@ -8,16 +8,22 @@ public class jumpmovement : MonoBehaviour
     public float GravityModifier = 1f;
     public bool IsOnGround = true;
     public float Speed = 10f;
+    public float OutOfBounds = -10f;
 
     private float _horizontalInput;
     private float _forwardInput;
     private Rigidbody _playerRigidbody;
+    private Vector3 _startingPosition;
+    private Vector3 _checkpointPosition;
+    private bool _isAtCheckpoint = false;
 
     // Start is called before the first frame update
     void Start()
     {
+
         _playerRigidbody = GetComponent<Rigidbody>();
         Physics.gravity *= GravityModifier;
+        _startingPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -32,7 +38,17 @@ public class jumpmovement : MonoBehaviour
             IsOnGround = false;
 
         }
-
+        if (transform.position.y < OutOfBounds)
+        {
+            if (_isAtCheckpoint)
+            {
+                transform.position = _checkpointPosition;
+            }
+            else
+            {
+                transform.position = _startingPosition;
+            }
+        }
     }
     void FixedUpdate()
     {
@@ -47,5 +63,25 @@ public class jumpmovement : MonoBehaviour
         {
             IsOnGround = true;
         }
+        if (collision.Gameobject./ Compare)
+        {
+             
+        }
+
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            _isAtCheckpoint = true;
+            _startingPosition = other.gameObject.transform.position;
+        }
+        if(other.gameObject.CompareTag("Endpoint"))
+        {
+            transform.position = _startingPosition;
+
+        }
+
+    }
+
 }
